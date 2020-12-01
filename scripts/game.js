d1d7e09c5bed38489fb22f36122bfad02027c0ee
Maiki <maiki.moseley@gmail.com>
2020-12-01 16:51:01 -0600

    add color selection

--- scripts/game.js
+++ scripts/game.js
@@ -1,1 +1,6 @@
 const gameInterface = $("#game-interface").remove();
+($("button")).on("click", function(){
+    document.getElementById("blob-color").setAttribute("style",`fill:${$("#color").val()}`);
+    $("#blob-initialization").remove();
+    $("body").append(gameInterface);
+})
