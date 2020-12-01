72964cfecdf5110b19bbddda1059a5a7323c6a80
Maiki <maiki.moseley@gmail.com>
2020-12-01 17:33:58 -0600

    finish blob initialization html/css

--- scripts/game.js
+++ scripts/game.js
@@ -1,6 +1,16 @@
 const gameInterface = $("#game-interface").remove();
 ($("button")).on("click", function(){
-    document.getElementById("blob-color").setAttribute("style",`fill:${$("#color").val()}`);
+    $("#name-display").text($("#name").val());
+    $("#name-container").css("transform","translateY(0%");
+    $("#blob-color").attr("style",`fill:${$("#color").val()}`);
     $("#blob-initialization").remove();
     $("body").append(gameInterface);
+    $("#property-display").css("transform","translateX(0%)");
+    $("#emote").text("!");
+    $("#emote-container").css({"animation":"jump 2","animation-duration":".2s","animation-timing-function":"linear","animation-direction":"alternate"});
+    setTimeout(eraseExcla, 1200);
 })
+
+const eraseExcla = function(){
+    $("#emote").text("");
+}
